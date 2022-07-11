6dcbee8e8d9873ce2886b4338d2b6b05ae65ea88
moneruzzaman <moneruzzamanju@gmail.com>
2022-07-11 10:49:48 +0600

    Create a funtion named test for adding two number by passing parameter

---
+++ Date_11.07.222/script.js
@@ -0,0 +1,14 @@
+function test(x, y)
+{
+    var z = x + y;
+    document.write(z);
+}
+
+test(10, 20);
+document.write('<br/>================<br/>');
+
+test(100, 200);
+document.write('<br/>================<br/>');
+
+test(1000, 2000);
+document.write('<br/>================<br/>');
